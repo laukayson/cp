b4bf92e067c76170f9440541bcd31710d70a6f2d
laukayson <laukayson@gmail.com>
2025-03-21 15:20:23 +0800

    Create missingnumber.cpp

---
+++ CSES/Problemset/Introductory Problems/missingnumber.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    fill(arr,arr+n,n+1);
+    for(int i=0;i<n-1;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        if((i+1)!=arr[i]){
+            cout<<i+1;
+            break;
+        }
+    }
+}
